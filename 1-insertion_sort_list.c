fefe649ad220caf118f090baabf1ab99e696b357
Zandarh <ubaksxander@gmail.com>
2022-07-15 13:13:36 +0100

    sort updated

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,20 +10,27 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *temp;
 
+	/* checking if the list is empty or has only one node */
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	temp = *list;
 
+	/* Looping till the end node is reached */
 	while (temp != NULL)
 	{
 		current = temp;
 
+		/* checking that the current node has a previous node */
+		/* And that the data in previous node is > the that of curent node */
+		/* This *cuurent will always point at the same node, hence... */
+		/* ... an indirect way of doing a current--, if that makes sense */
 		while (current->prev != NULL && current->prev->n > current->n)
 		{
 			swap_doubly_linked_list(list, current);
 		}
 
+		/* Moving the temp node upward until it reaches the last node */
 		temp = temp->next;
 	}
 
@@ -39,18 +46,23 @@ listint_t *swap_doubly_linked_list(listint_t **head, listint_t *node)
 {
 	listint_t *next_node, *prev_node;
 
+	/* When swapping the first two nodes */
 	if (node->prev == *head)
 		*head = node;
 
+	/* two pointers to point before and after the current node */
 	next_node = node->next;
 	prev_node = node->prev;
 
+	/* when *prev_node points to any node except the first node */
 	if (prev_node->prev != NULL)
 		prev_node->prev->next = node;
 
+	/* when the swapping the last node. *next_node == NULL */
 	if (next_node != NULL)
 		next_node->prev = node->prev;
 
+	/* Swapping the links between four nodes */
 	node->prev = prev_node->prev;
 	node->next = prev_node;
 	prev_node->next = next_node;

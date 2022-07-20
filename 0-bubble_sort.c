c459c01369ac17e4b01d87ca2fc576125d549b59
Zandarh <ubaksxander@gmail.com>
2022-07-20 03:38:59 +0100

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,24 @@
 #include "sort.h"
 
 /**
+ * swap - swaps the elemnts in a array
+ * @array: pointer to the aray
+ * @first: first element
+ * @second: second element
+ * Return: Void
+ */
+
+void swap(int *array, size_t first, size_t second)
+{
+	int temp;
+
+	temp = array[first];
+	array[first] = array[second];
+	array[second] = temp;
+}
+
+
+/**
  * bubble_sort - sorts an array of integers in ascending order
  * @array: pointer to the array
  * @size: size of the array
@@ -10,7 +28,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t j;
-	int temp, flag = 1;
+	int flag = 1;
 
 	if (size < 2)
 		return;
@@ -29,9 +47,7 @@ void bubble_sort(int *array, size_t size)
 				/* Changing the value of flag indicating a swap */
 				flag = 1;
 				/* Swapping them if it is greater */
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap(array, j, j + 1);
 				print_array(array, size);
 			}
 		}
